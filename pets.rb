4b9c06a266ea041a570d0cbd30872b1137347102
Valentin Comte <valentin@traxair.com>
2015-10-27 10:55:23 +0000

    second part

--- pets.rb
+++ pets.rb
@@ -1,24 +1,52 @@
-class Cat
+class Pet
+  def speak
+    puts 'noise'
+  end
+end
+
+class Cat < Pet
   def speak
     puts 'Meeow!'
   end
 end
 
-class Dog
+class Dog < Pet
   def speak
     puts 'Woof!'
   end
+end
+
+class RodentPet < Pet
+  def speak
+    puts 'squeak!'
+  end
+end
+
+class Mouse < RodentPet
+end
+
+class Gerbil < RodentPet
+end
+
+class Fox < Pet
 end
 
 class PetLover
   attr_accessor :array
   def initialize
     @array = []
-    for i in 0..9 do
-      if Random.rand(2) == 0
+    for i in 0..100 do
+      rand = Random.rand(5)
+      if rand == 0
         @array.push(Dog.new)
-      else
+      elsif rand == 1
         @array.push(Cat.new)
+      elsif rand == 2
+        @array.push(Mouse.new)
+      elsif rand == 3
+        @array.push(Gerbil.new)
+      elsif rand == 4
+        @array.push(Fox.new)
       end
     end
   end
