1d6ecc59c5463937178ae423cf100383111e51d8
Valentin Comte <valentin@traxair.com>
2015-10-27 11:44:17 +0000

    third part

--- pets.rb
+++ pets.rb
@@ -1,31 +1,57 @@
 class Pet
+  def initialize
+    @food = "many things"
+    @home = "in my home"
+  end
   def speak
-    puts 'noise'
+    puts "noise"
+  end
+  def daily_routine
+    puts "I am #{self.class.name}"
+    puts "I eat #{@food}"
+    3.times.collect{self.speak}
+    puts "I sleep in #{@home}"
   end
 end
 
 class Cat < Pet
+  def initialize
+    @food = "fish"
+    @home = "my basket"
+  end
   def speak
-    puts 'Meeow!'
+    puts "Meeow!"
   end
 end
 
 class Dog < Pet
+  def initialize
+    @food = "meat"
+    @home = "my kennel"
+  end
   def speak
-    puts 'Woof!'
+    puts "Woof!"
   end
 end
 
 class RodentPet < Pet
   def speak
-    puts 'squeak!'
+    puts "squeak!"
   end
 end
 
 class Mouse < RodentPet
+  def initialize
+    @food = "cheese"
+    @home = "my hole in the wall"
+  end
 end
 
 class Gerbil < RodentPet
+  def initialize
+    @food = "fruits"
+    @home = "the garden"
+  end
 end
 
 class Fox < Pet
@@ -35,7 +61,7 @@ class PetLover
   attr_accessor :array
   def initialize
     @array = []
-    for i in 0..100 do
+    for i in 0..10 do
       rand = Random.rand(5)
       if rand == 0
         @array.push(Dog.new)
@@ -55,7 +81,7 @@ end
 def main()
   pets = PetLover.new
   for pet in pets.array do
-    pet.speak
+    pet.daily_routine
   end
 end
 
